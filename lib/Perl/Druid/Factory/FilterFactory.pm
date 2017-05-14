c98acbf61bfc34e8eab267bb8c9b73fb93fde330
Gaurav Kohli <gaurav.in@gmail.com>
2017-05-14 20:39:49 +0200

    Added regex filter extended filterfactor to implement all the logical filters and regex filter

--- lib/Perl/Druid/Factory/FilterFactory.pm
+++ lib/Perl/Druid/Factory/FilterFactory.pm
@@ -1,27 +1,57 @@
 package Perl::Druid::Factory::FilterFactory;
 use Moo;
 
+use Perl::Druid::Filter::RegexFilter;
 use Perl::Druid::Filter::SelectorFilter;
 use Perl::Druid::Filter::Logical::AndFilter;
+use Perl::Druid::Filter::Logical::OrFilter;
+use Perl::Druid::Filter::Logical::NotFilter;
 
 sub selectorFilter {
    my $self = shift;
    my ( $dimension, $value) = @_;
-   
+
    return Perl::Druid::Filter::SelectorFilter->new(
 		dimension	=>	$dimension,
 		value		=>	$value
 	);
 }
 
+sub regexFilter {
+   my $self = shift;
+   my ( $dimension, $pattern) = @_;
+
+   return Perl::Druid::Filter::RegexFilter->new(
+		dimension	=>	$dimension,
+		pattern		=>	$pattern
+	);
+}
+
 sub andFilter {
 	my $self = shift;
 	my $fields = shift;
-	
-	return Perl::Druid::Filter::Logical::AndFilter->new(
-		fields		=>	 $fields 
-	);
 
+	return Perl::Druid::Filter::Logical::AndFilter->new(
+		fields		=>	 $fields
+	);
+}
+
+sub orFilter {
+	my $self = shift;
+	my $fields = shift;
+
+	return Perl::Druid::Filter::Logical::OrFilter->new(
+		fields		=>	 $fields
+	);
+}
+
+sub notFilter {
+	my $self = shift;
+	my $fields = shift;
+
+	return Perl::Druid::Filter::Logical::NotFilter->new(
+		fields		=>	 $fields
+	);
 }
 
 1;
---
+++ lib/Perl/Druid/Filter/RegexFilter.pm
@@ -0,0 +1,22 @@
+package Perl::Druid::Filter::RegexFilter;
+use Moo;
+
+extends 'Perl::Druid::Filter';
+
+sub type 	{ 'regex' }
+has pattern	=> (is => 'ro');
+
+sub build {
+	my $self = shift;
+	
+	my $filter = {
+		'type' 		=> $self->type,
+		'dimension' => $self->dimension,
+		'pattern' 	=> $self->pattern,
+	};
+
+	return $filter;
+}
+
+1;
+
