296218850d29ecbfce7552dcf6b22aa663eecb1f
Sawyer X <xsawyerx@cpan.org>
2017-07-07 20:10:25 -0400

    Fix typo bug: '$6', not '%6'

--- lib/Druid/Util.pm
+++ lib/Druid/Util.pm
@@ -15,7 +15,7 @@ sub iso8601_yyyy_mm_dd_hh_mm_ss {
     my $iso_date = shift;
 
     if( $iso_date =~ m/^([0-9]{4})-([0-9]{2})-([0-9]{2})T([0-9]{2}):([0-9]{2}):([0-9]{2}).([0-9]{3})Z$/ ){
-        return  sprintf '%s-%02d-%02d %02d:%02d:%02d', $1, $2, $3, $4, $5, %6;
+        return  sprintf '%s-%02d-%02d %02d:%02d:%02d', $1, $2, $3, $4, $5, $6;
     } else {
         die "\'$iso_date\' not a valid ISO8601 format.\n";
     }
@@ -26,7 +26,7 @@ sub yyyy_mm_dd_hh_mm_ss_iso8601 {
     my $yyyy_mm_dd_hh_mm_ss = shift;
 
     if( $yyyy_mm_dd_hh_mm_ss =~ m/^([0-9]{4})-?([0-9]{2})-?([0-9]{2})\s*([0-9]{2}):?([0-9]{2}):?([0-9]{2})$/ ){
-        return  sprintf '%s-%02d-%02dT%02d:%02d:%02d', $1, $2, $3, $4, $5, %6;
+        return  sprintf '%s-%02d-%02dT%02d:%02d:%02d', $1, $2, $3, $4, $5, $6;
     } else {
         die "\'$yyyy_mm_dd_hh_mm_ss\' not a valid YYYYMMDDHHMMSS or YYYY-MM-DD HH:MM:SS format.\n";
     }
